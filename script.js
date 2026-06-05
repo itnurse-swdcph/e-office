a3328848fb34f147c42a41f2d0b13774aceca941
itnurse-swdcph <itnurse.swdcph@gmail.com>
2026-06-05 22:23:18 +0700

    Update script.js

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ function systemApp() {
     },
 
     generateThaiDate() {
-      const months = ["ม.ค.", "ก.พ.", "มี.ค.", "เม.ย.", "พ.ค.", "มิ.ย.", "ก.ค.", "ส.ค.", "ก.ย.", "ต.ค.", "พ.ย.", "ธ.ค."];
+      const months = ["มกราคม", "กุมภาพันธ์", "มีนาคม", "เมษายน", "พฤษภาคม", "มิถุนายน", "กรกฎาคม", "สิงหาคม", "กันยายน", "ตุลาคม", "พฤศจิกายน", "ธันวาคม"];
       const d = new Date();
       this.thaiDateNow = `${d.getDate()} ${months[d.getMonth()]} ${d.getFullYear() + 543}`;
     },
